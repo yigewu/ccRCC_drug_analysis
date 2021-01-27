167f731ef3c45ce893a1f362581fc45c048dec76
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-01-27 13:47:56 -0600

    make seurat object v2

--- treatment_data/tumor_volume/plotting/lineplot_avgrelativetumorvolume_change_cabo_sapa_GU374B_B1.R
+++ treatment_data/tumor_volume/plotting/lineplot_avgrelativetumorvolume_change_cabo_sapa_GU374B_B1.R
@@ -18,6 +18,8 @@ dir.create(dir_out)
 # input dependencies --------------------------------------------------
 ## input the treatment groups
 treatmentgroups_process <- c("CT", "Cab", "Sap", "Cab+Sap")
+## set sample id
+
 ## input formatted measurement data
 avg_relative_volume_df <- fread(data.table = F, input = "./Resources/Analysis_Results/treatment_data/tumor_volume/process_tumor_volume/20210126.v1/GU374B_B1_KS_20210126/GU374B_B1_KS_20210126.RelativeTumoreVolume.Average.ByTreatmentGroup.20210126.v1.tsv")
 
@@ -30,7 +32,9 @@ plot_data_wide_df <- avg_relative_volume_df[, c("Treatment_Days", treatmentgroup
 plot_data_long_df <- reshape2::melt(data = plot_data_wide_df, id.var = c("Treatment_Days"))
 plot_data_long_df <- plot_data_long_df %>%
   rename(Avg_Relative_Volume = value) %>%
-  rename(Treatment_Group = variable)
+  rename(Treatment_Group = variable) %>%
+  mutate(Avg_Relative_Volume_Change = (Avg_Relative_Volume - 100))
+  
 
 # specify plotting parameters ---------------------------------------------
 color_red <- RColorBrewer::brewer.pal(n = 7, name = "Set1")[1]
@@ -38,13 +42,18 @@ color_green <- RColorBrewer::brewer.pal(n = 7, name = "Set1")[3]
 color_yellow <- RColorBrewer::brewer.pal(n = 7, name = "Set2")[6]
 color_grey <- "grey50"
 
-# plot --------------------------------------------------------------------
-p <- ggplot(data = plot_data_long_df, mapping = aes(x = Treatment_Days, y = Avg_Relative_Volume, group = Treatment_Group, color = Treatment_Group))
+# plot 0 - 100 --------------------------------------------------------------------
+p <- ggplot(data = plot_data_long_df, mapping = aes(x = Treatment_Days, y = Avg_Relative_Volume_Change, group = Treatment_Group, color = Treatment_Group))
 p <- p + geom_line()
 p <- p + geom_point()
 p <- p + scale_color_manual(values = c("CT" = color_grey, "Cab" = color_red, "Sap" = color_green, "Cab+Sap" = color_yellow))
+p <- p + scale_y_continuous(limits = c(-40, 140), breaks = seq(-40, 120, 20))
+p <- p + scale_x_continuous(expand = c(0,0))
 p <- p + theme_classic()
+p <- p + geom_hline(aes(yintercept = 0), linetype = 2)
+p <- p + geom_hline(aes(yintercept = 100))
 p
+file2write <- paste0(dir_out, )
 
 # save output -------------------------------------------------------------
 
