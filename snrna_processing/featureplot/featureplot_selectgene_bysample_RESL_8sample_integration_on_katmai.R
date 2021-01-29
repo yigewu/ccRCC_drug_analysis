6148a36390d7454ccdcf0811654d9c79c682a3d8
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-01-29 09:07:49 -0600

    featureplot v2

---
+++ snrna_processing/featureplot/featureplot_selectgene_bysample_RESL_8sample_integration_on_katmai.R
@@ -0,0 +1,71 @@
+# Yige Wu @WashU Apr 2020
+## for making featureplot for RESL5_4sample_integration
+
+# set up libraries and output directory -----------------------------------
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set time stamp for log file
+timestamp <- paste0(run_id, ".", format(Sys.time(), "%H%M%S"))
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_Drug/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_Drug/"
+setwd(dir_base)
+source("./ccRCC_drug_analysis/load_pkgs.R")
+source("./ccRCC_drug_analysis/functions.R")
+source("./ccRCC_drug_analysis/variables.R")
+library(Seurat)
+## set output directory
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+
+# set dependencies --------------------------------------------------------
+## set the path to the rds file for integrated object
+path_rds <- "./Resources/Analysis_Results/snrna_processing/integration/run_8sample_integration_withanchor_on_katmai/20210128.v1/RESL_8sample_integration.withanchor.20210128.v1.RDS"
+## input RDS file
+srat <- readRDS(file = path_rds)
+DefaultAssay(srat) <- "RNA"
+
+## input marker gene table
+genes_plot_df <- data.frame(Gene_Symbol = c("VIM", "VEGFA", "Vegfa", "Flt1", "HGF", "Hgf"), Species = c("Human", "Human", "Mouse", "Mouse", "Human", "Mouse"))
+## set the minimal % of cells expresssing the gene
+min.exp.pct <- 0
+# make plot ---------------------------------------------------------------
+## make feature name
+genes_plot_df <- genes_plot_df %>%
+  mutate(feature_name = ifelse(Species == "Human", paste0("GRCh38-", Gene_Symbol), paste0("mm10---", Gene_Symbol)))
+## get feature names in RNA count data
+featurenames <-  intersect(genes_plot_df$feature_name, srat@assays$RNA@counts@Dimnames[[1]])
+featurenames <- unique(featurenames)
+
+# modify srat object ------------------------------------------------------
+
+
+
+for (featurename in featurenames) {
+  p <- FeaturePlot(object = srat, features = featurename, 
+                   min.cutoff = "q10", max.cutoff = "q90", sort.cell = TRUE,
+                   cols = c("grey", "red"), reduction = "umap", label = T, split.by = "orig.ident", ncol = 4)
+  
+  # save output -------------------------------------------------------------
+  file2write <- paste0(dir_out, featurename, ".featureplot", ".png")
+  png(filename = file2write, width = 3000, height = 1500, res = 150)
+  print(p)
+  dev.off()
+}
+
+
