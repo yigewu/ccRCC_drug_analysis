369ecff76094b0454abf75700f2ca794dc79a321
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-02-10 08:17:21 -0600

    fetch data mouse cells v1

---
+++ snrna_processing/fetch_data/fetch_data_mousecells_8sample_integration_withanchor_on_katmai.R
@@ -0,0 +1,50 @@
+# Yige Wu @WashU Apr 2020
+## for making dimplot for RESL10_4sample_integration
+
+# set up libraries and output directory -----------------------------------
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set time stamp for log file
+timestamp <- paste0(run_id, ".", format(Sys.time(), "%H%M%S"))
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_Drug/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_Drug/"
+setwd(dir_base)
+source("./ccRCC_drug_analysis/load_pkgs.R")
+source("./ccRCC_drug_analysis/functions.R")
+source("./ccRCC_drug_analysis/variables.R")
+library(Seurat)
+## set output directory
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+
+# set dependencies --------------------------------------------------------
+## set the path to the rds file for integrated object
+path_rds <- "./Resources/Analysis_Results/snrna_processing/integration/run_mousecells_8sample_integration_withanchor_on_katmai/20210208.v1/MouseCells_8sample_integration.withanchor.20210208.v1.RDS"
+## input RDS file
+srat <- readRDS(file = path_rds)
+
+# fetch dats --------------------------------------------------------------
+fetcheddata_df <- FetchData(object = srat, vars = c("orig.ident", "seurat_clusters", "UMAP_1", "UMAP_2"))
+fetcheddata_df$barcode <- rownames(fetcheddata_df)
+
+# write output ------------------------------------------------------------
+file2write <- paste0(dir_out, "MouseCells_8sample", ".umap_data.", run_id, ".tsv")
+write.table(x = fetcheddata_df, file = file2write, row.names = T, quote = F, sep = "\t")
+
+
