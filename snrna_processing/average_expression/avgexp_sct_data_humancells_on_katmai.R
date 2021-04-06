a205de783d2a74266eb5ee6b981d6d6219830744
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-04-06 11:27:53 -0500

    average expression all human cells v1

---
+++ snrna_processing/average_expression/avgexp_sct_data_humancells_on_katmai.R
@@ -0,0 +1,63 @@
+# Yige Wu @WashU Apr 2020
+
+# set up libraries and output directory -----------------------------------
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set time stamp for log file
+timestamp <- paste0(run_id, ".", format(Sys.time(), "%H%M%S"))
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_Drug/"
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_Drug/"
+setwd(dir_base)
+source("./ccRCC_drug_analysis/load_pkgs.R")
+source("./ccRCC_drug_analysis/functions.R")
+source("./ccRCC_drug_analysis/variables.R")
+library(Seurat)
+## set output directory
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+## set log file
+sink(file = paste0(dir_out, "Log.", timestamp, ".txt"))
+
+# set dependencies --------------------------------------------------------
+## set the path to the rds file for integrated object
+path_rds <- "./Resources/Analysis_Results/snrna_processing/integration/run_humancells_8sample_integration_withanchor_on_katmai/20210208.v1/Humancells_8sample_integration.withanchor.20210208.v1.RDS"
+## input RDS file
+srat <- readRDS(file = path_rds)
+print("Finish reading RDS file")
+# spcify assay
+assay_process <- "SCT"
+slot_process <- "data"
+cat(paste0("Assay: ", assay_process, "\n"))
+
+# modify srat object ------------------------------------------------------
+srat@meta.data$Id_Manual_Cluster <- "Tumor_cells"
+## change ident
+Idents(srat) <- "Id_Manual_Cluster"
+
+## run average expression
+aliquot.averages <- AverageExpression(srat, assays = assay_process, slot = slot_process)
+print("Finish running AverageExpression!\n")
+cat("###########################################\n")
+
+## write output
+file2write <- paste0(dir_out, "HumanCells.", "AverageExpression.", run_id, ".tsv")
+write.table(aliquot.averages, file = file2write, quote = F, sep = "\t", row.names = T)
+cat("Finished saving the output\n")
+cat("###########################################\n")
+sink()
