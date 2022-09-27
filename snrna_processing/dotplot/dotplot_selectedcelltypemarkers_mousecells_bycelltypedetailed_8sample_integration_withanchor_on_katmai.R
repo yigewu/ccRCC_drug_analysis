c444352af2a84284817a1f7f341c47fd452c8c7e
yigewu <yigewu@wustl.edu>
2022-09-27 10:47:10 +0800

    dot plot for mouse cells

---
+++ snrna_processing/dotplot/dotplot_selectedcelltypemarkers_mousecells_bycelltypedetailed_8sample_integration_withanchor_on_katmai.R
@@ -0,0 +1,160 @@
+# Yige Wu @WashU Apr 2020
+## for making dimplot for RESL5_4sample_integration
+
+# set up libraries and output directory -----------------------------------
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set time stamp for log file
+timestamp <- paste0(run_id, ".", format(Sys.time(), "%H%M%S"))
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_Drug/"
+setwd(dir_base)
+packages = c(
+  "rstudioapi",
+  "plyr",
+  "dplyr",
+  "stringr",
+  "reshape2",
+  "data.table",
+  "ggplot2",
+  "Seurat"
+)
+
+for (pkg_name_tmp in packages) {
+  library(package = pkg_name_tmp, character.only = T)
+}
+library(Seurat)
+## set output directory
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+
+# set dependencies --------------------------------------------------------
+## set the path to the rds file for integrated object
+path_rds <- "./Resources/Analysis_Results/snrna_processing/integration/run_mousecells_8sample_integration_withanchor_on_katmai/20210208.v1/MouseCells_8sample_integration.withanchor.20210208.v1.RDS"
+## input RDS file
+srat <- readRDS(file = path_rds)
+DefaultAssay(srat) <- "RNA"
+##
+barcode2celltype_df <- fread(input = "./Resources/Analysis_Results/snrna_processing/map_barcode/map_barcode2celltype_mousecells_integration_withanchor/20210225.v1/MouseCells.Barcode2CellType.20210225.v1.tsv", data.table = F)
+## input marker gene table
+gene2celltype_df <- fread("./Resources/Knowledge/Gene_Lists/Cell_Type_Marker_Genes/Mouse.Gene2CellType.20210224.v2.tsv", data.table = F)
+## set the minimal % of cells expresssing the gene
+min.exp.pct <- 10
+
+# process seurat object ---------------------------------------------------
+srat@meta.data$cell_group <- mapvalues(x = rownames(srat@meta.data), 
+                                       from = barcode2celltype_df$Barcode_Integrated, 
+                                       to = barcode2celltype_df$Cell_Type.Detailed)
+table(srat@meta.data$cell_group)
+
+# get gene to plot --------------------------------------------------------
+# ## make feature name
+# gene2celltype_df <- gene2celltype_df %>%
+#   mutate(feature_name = Gene) %>%
+#   filter(!(Cell_Type_Group %in% c("Neprhon_Epithelium", "Urothelium")))
+# ## change ident
+# srat@meta.data$id_by_cluster_species <- paste0(srat@meta.data$seurat_clusters, "_", srat@meta.data$call)
+# Idents(srat) <- "id_by_cluster_species"
+# ## get feature names in RNA count data
+# featurenames <-  intersect(gene2celltype_df$feature_name, srat@assays$RNA@data@Dimnames[[1]])
+# featurenames <- unique(featurenames)
+# ## get the pct expressed for each gene in each cluster
+# p <- DotPlot(object = srat, features = featurenames, col.min = 0, assay = "RNA")
+# expdata_df <- p$data
+# # print(expdata_df[1:4, 1:4])
+# ## transform the dataframe to matrix to better filter out genes with too low expressin
+# plot_matrix <- dcast(data = expdata_df, formula = features.plot ~ id, value.var = "pct.exp")
+# ## filter for genes that are expressed in >XX% (min.exp.pct) of one cluster at least
+# ## replot with the filtered genes plus malignant cell marker genes
+# featurenames_filtered <- as.vector(plot_matrix[rowSums(plot_matrix[,unique(as.vector(expdata_df$id))] > min.exp.pct) >= 1, "features.plot"])
+# print(length(featurenames_filtered))
+# cat("Finished making plot data!\n\n\n")
+
+# make scaled dotplot ------------------------------------------------------------
+featurenames_filtered <- c("Ptprc","Flt1", "Pecam1", "Tek", "Pdgfrb", "Tgfbi", "Col14a1", "Mrc1", "F13a1", "Col3a1", "Dcn", "Plac8", "Acta2", "Mylk")
+cat("Start plotting scaled!\n\n\n")
+p <- DotPlot(object = srat, features = featurenames_filtered, col.min = 0, assay = "RNA")
+p$data$gene_cell_type_group <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type_Group)
+p$data$gene_cell_type1 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type1)
+p$data$gene_cell_type2 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type2)
+p$data$gene_cell_type3 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type3)
+p$data$gene_cell_type4 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type4)
+# p <- p + RotatedAxis()
+p <- p + facet_grid(.~gene_cell_type_group + gene_cell_type1 + gene_cell_type2 + gene_cell_type3 + gene_cell_type4, scales = "free", space = "free", drop = T)
+p <- p + theme(panel.spacing = unit(0, "lines"),
+               strip.background = element_blank(),
+               panel.border = element_rect(colour = "black"),
+               panel.grid.major = element_line(colour = "grey50"),
+               strip.text.x = element_text(angle = 0, vjust = 0.5),
+               axis.text.x = element_text(angle = 90, size = 15, face = "bold"),
+               strip.placement = "outside")
+cat("Finished plotting scaled!\n\n\n")
+file2write <- paste0(dir_out, "dotplot.", "scaled.", "pdf")
+pdf(file2write, width = 10, height = 6, useDingbats = F)
+print(p)
+dev.off()
+cat("Finished writing pdf!\n\n\n")
+# file2write <- paste0(dir_out, "dotplot.", "scaled.", "png")
+# png(filename = file2write, width = 3000, height = 1500, res = 150)
+# print(p)
+# dev.off()
+# cat("Finished writing png!\n\n\n")
+
+# plot not scaled -------------------------------------------------------------
+cat("Starting not-scaled plot!\n\n\n")
+plotdata_df <- expdata_df %>%
+  filter(features.plot %in% featurenames_filtered)
+expvalue_top <- quantile(x = plotdata_df$avg.exp, probs = 0.95)
+plotdata_df <- plotdata_df %>%
+  mutate(expvalue_plot = ifelse(avg.exp >= expvalue_top, expvalue_top, avg.exp))
+## add facet
+plotdata_df$gene_cell_type_group <- plyr::mapvalues(plotdata_df$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type_Group)
+plotdata_df$gene_cell_type1 <- plyr::mapvalues(plotdata_df$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type1)
+plotdata_df$gene_cell_type2 <- plyr::mapvalues(plotdata_df$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type2)
+plotdata_df$gene_cell_type3 <- plyr::mapvalues(plotdata_df$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type3)
+plotdata_df$gene_cell_type4 <- plyr::mapvalues(plotdata_df$features.plot, from = gene2celltype_df$feature_name, to = gene2celltype_df$Cell_Type4)
+## plot
+p <- ggplot()
+p <- p + geom_point(data = plotdata_df, mapping = aes(x = features.plot, y = id, color = expvalue_plot, size = pct.exp), shape = 16)
+p <- p + scale_color_gradientn(colours = rev(RColorBrewer::brewer.pal(n = 9, name = "Spectral")[1:5]), guide = guide_legend(direction = "horizontal", nrow = 2, byrow = T))
+p <- p + scale_size_continuous(range = c(0, 8), name="% Expressed", guide = guide_legend(direction = "horizontal"))
+p <- p + facet_grid(.~gene_cell_type_group + gene_cell_type1 + gene_cell_type2 + gene_cell_type3 + gene_cell_type4, scales = "free", space = "free", drop = T)
+p <- p + theme(axis.text.x = element_text(angle = 90, size = 10))
+p <- p + theme(axis.text.y = element_text(size = 12))
+p <- p + theme(panel.spacing = unit(0, "lines"), 
+               panel.grid.major = element_line(colour = "grey80"), 
+               panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+               panel.background = element_blank())
+p <- p + theme(strip.background = element_blank(),
+               strip.text.x = element_text(angle = 0, vjust = 0.5))
+p <- p + theme(axis.title = element_blank())
+p <- p + labs(colour = "Expression value")
+p <- p + theme(legend.position = "bottom")
+cat("Finished plotting not-scaled!\n\n\n")
+# file2write <- paste0(dir_out, "dotplot.", "not_scaled.", "png")
+# png(filename = file2write, width = 3000, height = 1700, res = 150)
+# print(p)
+# dev.off()
+# cat("Finished writing png!\n\n\n")
+file2write <- paste0(dir_out, "dotplot.", "scaled.", "pdf")
+pdf(file2write, width = 10, height = 6, useDingbats = F)
+print(p)
+dev.off()
+cat("Finished writing pdf!\n\n\n")
+
+
--- snrna_processing/signature_scores/plotting/complexheatmap_humancells_8sampleintegrated_res05_bysample_bycluster_sigscores_scaled_horizontal.R
+++ snrna_processing/signature_scores/plotting/complexheatmap_humancells_8sampleintegrated_res05_bysample_bycluster_sigscores_scaled_horizontal.R
@@ -18,6 +18,18 @@ for (pkg_name_tmp in packages) {
   library(package = pkg_name_tmp, character.only = T)
 }
 
+# prepare for plotting ----------------------------------------------------
+fontsize_plot <- 30
+anno_fontsize <- 30
+cellwidth <- 3
+source("./ccRCC_drug_analysis/functions.R")
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set output directory
+dir_out <- paste0(makeOutDir(), run_id, "/")
+dir.create(dir_out)
+
 # input dependencies ------------------------------------------------------
 ## input median(?) signature scores per cluster
 results_df <- fread(data.table = F, input = "./Resources/Analysis_Results/snrna_processing/signature_scores/other/make_median_signaturescores_bygeneset_bycluster_bysample/20220918.v1/median_scores.res05.bycluster.bysample.humancells.8sampleintegrated.20220918.v1.tsv")
@@ -49,7 +61,7 @@ plotdata_long_df <- plotdata_long_df %>%
   mutate(cluster = as.numeric(cluster)) %>%
   arrange(cluster) %>%
   mutate(y_plot = paste0(model, "_", treatment, "_", paste0("MC", (cluster+1))))
-
+unique(plotdata_long_df$y_plot)
 rm(results_df)
 plotdata_wide_df <- dcast(data = plotdata_long_df, formula = x_plot ~ y_plot, value.var = "value")
 plotdata_raw_mat <- as.matrix(plotdata_wide_df[,-1])
@@ -57,130 +69,169 @@ plotdata_raw_mat <- as.matrix(plotdata_wide_df[,-1])
 plotdata_mat <- apply(plotdata_raw_mat, 1, scale)
 rownames(plotdata_mat) <- colnames(plotdata_raw_mat)
 colnames(plotdata_mat) <- plotdata_wide_df$x_plot
+rm(plotdata_raw_mat)
+plotdata_mat1 <- cor(t(plotdata_mat), method = "spearman")
+
 ## reorder clusters
-clusters_ordered <- plotdata_long_df$y_plot
+clusters_ordered <- unique(plotdata_long_df$y_plot)
 plotdata_mat <- plotdata_mat[clusters_ordered,]
 row_ids <- rownames(plotdata_mat)
 column_ids <- colnames(plotdata_mat)
-row_labels_vec <- plotdata_long_df$y_plot
+row_labels_vec <- clusters_ordered
+col_labels_vec <- str_split_fixed(string = column_ids, pattern = "_", n = 2)[,2]
 rm(plotdata_long_df)
 rm(plotdata_wide_df)
-
-# make row labels ---------------------------------------------------------
-# row_labels_vec <- c("MC2:ER_signaling", "MC3:OXPHOS_high", "MC18:OXOHOS_high",
-#                     "MC4:DDR_dysregulated", "MC1:DDR_dysregulated", "MC8:DDR_dysregulated", "MC13:DDR_dysregulated",
-#                     "MC5:ER_signaling", "MC12:Cycling", "MC17:Multi-activated", "MC15:Cycling", 
-#                     "MC6:IL2-STAT5_signaling", "MC9:IFN_signaling", "MC10:Inflammatory", "MC14:Inflammatory+KRAS signaling",
-#                     "MC7:Hypoxic", "MC16:Multi-activated", "MC11:Multi-activated")
-# col_labels_vec <- gsub(x = column_ids, replacement = "", pattern = "HALLMARK_")
-# col_labels_vec <- tolower(col_labels_vec)
-# col_labels_vec[col_labels_vec == tolower("EPITHELIAL_MESENCHYMAL_TRANSITION")] <- "EMT"
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "uv_", replacement = "UV_")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "e2f_", replacement = "E2F_")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "g2m_", replacement = "G2M_")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "myc_", replacement = "MYC_")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "il6_jak_stat3", replacement = "IL6-JAK-STAT3")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "il2_stat5", replacement = "IL2-STAT5")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "pi3k_akt_mtor", replacement = "PI3K-AKT-mTOR")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "kras", replacement = "KRAS")
-# col_labels_vec[col_labels_vec == "tnfa_signaling_via_nfkb"] <- expression("TNF-alpha signaling via NFkB")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "mtorc1", replacement = "mTORC1")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "tgf_beta", replacement = "TGF-beta")
-# col_labels_vec <- gsub(x = col_labels_vec, pattern = "_", replacement = " ")
+model_ids_vec <- str_split_fixed(string = row_ids, pattern = "_", n = 3)[,1]
+treatment_vec <- str_split_fixed(string = row_ids, pattern = "_", n = 3)[,2]
+clusters_vec <- str_split_fixed(string = row_ids, pattern = "_", n = 3)[,3]
 
 # make colors -------------------------------------------------------------
 ## make colors for the heatmap body
 summary(as.vector(plotdata_mat))
-# colors_sig_zscore = circlize::colorRamp2(breaks = c(-1.5, 0, seq(0.2, 1.8, 0.2)), 
-#                                       colors = c("blue", "white", RColorBrewer::brewer.pal(n = 9, name = "YlOrRd")))
 colors_sig_zscore = circlize::colorRamp2(breaks = c(-2, 0, 2), 
                                       colors = c("purple", "black", "yellow"))
 ## 
 colors_correlation <- colorRamp2(c(-1, 0, 1), c("blue", "white", "red")) 
+## colors for model
+colors_model <- RColorBrewer::brewer.pal(n = 3, name = "Dark2")[c(1,2)]
+names(colors_model) <- c("RESL5", "RESL10")
+## colors for treatment
+color_red <- RColorBrewer::brewer.pal(n = 7, name = "Set1")[1]
+color_green <- RColorBrewer::brewer.pal(n = 7, name = "Set1")[3]
+color_yellow <- RColorBrewer::brewer.pal(n = 7, name = "Set2")[6]
+color_grey <- "grey50"
+colors_treatment <- c("CT" = color_grey, "Cab" = color_red, "Sap" = color_green, "CabSap" = color_yellow)
+## colors for clusters
+clusters_uniq_vec <- unique(clusters_vec)
+colors_bycluster <- Polychrome::palette36.colors(n = (length(clusters_uniq_vec)+1))[-2]
+names(colors_bycluster) <- clusters_uniq_vec
 
 # make row annotation -----------------------------------------------------
-# consistency_vec <-  mapvalues(x = row_ids, from = sigCorr_df$gene_set, to = as.vector(sigCorr_df$C)); consistency_vec <- as.numeric(consistency_vec)
-geneset_cat_vec <- mapvalues(x =  gsub(x = column_ids, replacement = "", pattern = "HALLMARK_"), from = hallmark_anno_df$`Hallmark Name`, to = as.vector(hallmark_anno_df$`Process Category`))
+row_ha <- rowAnnotation(model = anno_simple(x = model_ids_vec, col = colors_model[model_ids_vec], width = unit(3*cellwidth, "mm")),
+                        treatment = anno_simple(x = treatment_vec, col = colors_treatment[treatment_vec], width = unit(3*cellwidth, "mm")),
+                        cluster = anno_simple(x = clusters_vec, col = colors_bycluster[clusters_vec], width = unit(3*cellwidth, "mm")), 
+                        annotation_name_side = "top", annotation_name_gp = gpar(fontsize = anno_fontsize))
 
-# make column split ----------------------------------------------------------
-col_split_vec <- geneset_cat_vec
-col_split_vec <- factor(x = col_split_vec, levels = c("metabolic", "DNA damage", "proliferation", "immune", "development", "pathway", "signaling", "cellular component"))
-
-# plot --------------------------------------------------------------------
-fontsize_plot <- 14
-plotdata_mat1 <- cor(t(plotdata_mat), method = "spearman")
-cellwidth <- 5
+# plot version 2 --------------------------------------------------------------------
 p <- Heatmap(matrix = plotdata_mat1,
                  col = colors_correlation,
-             width = ncol(plotdata_mat1)*unit(cellwidth, "mm"), 
+             width = ncol(plotdata_mat1)*unit(cellwidth, "mm"),
              height = nrow(plotdata_mat1)*unit(cellwidth, "mm"),
-             show_row_names = T, cluster_rows = T, show_row_dend = F,
-             cluster_columns = T, column_labels = row_labels_vec, column_names_gp = gpar(fontsize = fontsize_plot), column_dend_side = "bottom",
-             # name = "Correlation", 
-             show_heatmap_legend = F)
+             show_row_names = F, cluster_rows = T, show_row_dend = F,
+             cluster_columns = T, column_labels = row_labels_vec, show_column_names = F,
+             # column_names_gp = gpar(fontsize = fontsize_plot), 
+             # column_dend_side = "bottom",
+             # name = "Correlation",
+             show_heatmap_legend = F, use_raster = T)
 p <- p + Heatmap(matrix = plotdata_mat, 
              col = colors_sig_zscore,
              cell_fun = function(j, i, x, y, w, h, fill) {
                if (plotdata_mat[i,j] >= quantile(plotdata_mat[,j], 0.75)+1.5*IQR(plotdata_mat[,j])) {
                  grid.text("*", x, y)
                }
-               # if (plotdata_mat[i,j] >= max(plotdata_mat[i,])) {
-               #   grid.rect(x = x, y = y, width = w, height = h, 
-               #             gp = gpar(col = "red", fill = NA))
-               # }
-               if (plotdata_mat[i,j] >= quantile(plotdata_mat[,i], 0.75)+1.5*IQR(plotdata_mat[,i])) {
-                 grid.rect(x = x, y = y, width = w, height = h, 
+               if (plotdata_mat[i,j] >= quantile(plotdata_mat[i,], 0.75)+1.5*IQR(plotdata_mat[i,])) {
+                 grid.rect(x = x, y = y, width = w, height = h,
                            gp = gpar(col = "red", fill = NA))
                }
              },
-             width = ncol(plotdata_mat)*unit(cellwidth, "mm"), 
+             width = ncol(plotdata_mat)*unit(3*cellwidth, "mm"), 
              height = nrow(plotdata_mat)*unit(cellwidth, "mm"),
-             cluster_rows = F,
-             show_row_names = T,
              # top_annotation = col_anno_obj,
              cluster_columns = T, show_column_dend = F,
-             # column_split = col_split_vec, column_title_gp = gpar(fontsize = fontsize_plot), column_title_rot = 90,
              cluster_column_slices = F, 
-             # column_labels = col_labels_vec, 
-             column_names_gp = gpar(fontsize = fontsize_plot),
-             # # left_annotation = row_anno_obj, 
+             column_labels = col_labels_vec,
+             column_names_gp = gpar(fontsize = fontsize_plot), column_names_side = "bottom",
+             ## row
+             cluster_rows = F,
+             show_row_names = F,
+             right_annotation = row_ha,
              row_names_side = "right", 
              row_labels = row_labels_vec,
              row_names_gp = gpar(fontsize = fontsize_plot),
-             show_heatmap_legend = F)
+             show_heatmap_legend = F, use_raster = T)
 
 list_lgd = list(
   Legend(col_fun = colors_correlation, 
          title = "Correlation",
-         title_gp = gpar(fontsize = fontsize_plot),
-         labels_gp = gpar(fontsize = fontsize_plot),
+         title_gp = gpar(fontsize = anno_fontsize),
+         labels_gp = gpar(fontsize = anno_fontsize),
          # legend_width = unit(4, "cm"),
          # legend_height = unit(4, "cm"),
-         direction = "vertical"),
+         direction = "horizontal"),
   Legend(col_fun = colors_sig_zscore, 
-         title = "Signature\nz-score",
-         title_gp = gpar(fontsize = fontsize_plot),
-         labels_gp = gpar(fontsize = fontsize_plot),
+         title = "Signature z-score",
+         title_gp = gpar(fontsize = anno_fontsize),
+         labels_gp = gpar(fontsize = anno_fontsize),
          # legend_width = unit(4, "cm"),
          # legend_height = unit(4, "cm"),
-         direction = "vertical"))
+         direction = "horizontal"),
+  Legend(labels = names(colors_treatment), 
+         title = "Treatment", 
+         legend_gp = gpar(fill = colors_treatment),
+         title_gp = gpar(fontsize = anno_fontsize),
+         labels_gp = gpar(fontsize = anno_fontsize),
+         nrow = 1),
+  Legend(labels = names(colors_bycluster), 
+         title = "Meta-cluster", 
+         title_gp = gpar(fontsize = anno_fontsize),
+         labels_gp = gpar(fontsize = anno_fontsize),
+         legend_gp = gpar(fill = colors_bycluster), nrow = 2))
 
-source("./ccRCC_drug_analysis/functions.R")
-## set run id
-version_tmp <- 1
-run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
-## set output directory
-dir_out <- paste0(makeOutDir(), run_id, "/")
-dir.create(dir_out)
 
-file2write <- paste0(dir_out, "heatmap", ".png")
-png(file2write, width = 4000, height = 4000, res = 150)
-p
-dev.off()
-
-file2write <- paste0(dir_out, "heatmap", ".pdf")
-pdf(file2write, width = 20, height = 20, useDingbats = F)
+file2write <- paste0(dir_out, "heatmap2", ".pdf")
+pdf(file2write, width = 40, height = 55, useDingbats = F)
 draw(object = p,
-     annotation_legend_side = "left", annotation_legend_list = list_lgd)
+     annotation_legend_side = "top", annotation_legend_list = list_lgd)
 dev.off()
 
+# plot version 1 --------------------------------------------------------------------
+# p <- Heatmap(matrix = plotdata_mat, 
+#              col = colors_sig_zscore,
+#              cell_fun = function(j, i, x, y, w, h, fill) {
+#                if (plotdata_mat[i,j] >= quantile(plotdata_mat[,j], 0.75)+1.5*IQR(plotdata_mat[,j])) {
+#                  grid.text("*", x, y)
+#                }
+#                if (plotdata_mat[i,j] >= quantile(plotdata_mat[i,], 0.75)+1.5*IQR(plotdata_mat[i,])) {
+#                  grid.rect(x = x, y = y, width = w, height = h,
+#                            gp = gpar(col = "red", fill = NA))
+#                }
+#              },
+#              width = ncol(plotdata_mat)*unit(cellwidth, "mm"), 
+#              height = nrow(plotdata_mat)*unit(cellwidth, "mm"),
+#              cluster_rows = T,
+#              show_row_names = T,
+#              # top_annotation = col_anno_obj,
+#              cluster_columns = T, show_column_dend = F,
+#              # column_split = col_split_vec, column_title_gp = gpar(fontsize = fontsize_plot), column_title_rot = 90,
+#              cluster_column_slices = F, 
+#              # column_labels = col_labels_vec, 
+#              column_names_gp = gpar(fontsize = fontsize_plot),
+#              # # left_annotation = row_anno_obj, 
+#              row_names_side = "right", 
+#              row_labels = row_labels_vec,
+#              row_names_gp = gpar(fontsize = fontsize_plot),
+#              show_heatmap_legend = F, use_raster = T)
+# 
+# list_lgd = list(
+#   Legend(col_fun = colors_sig_zscore, 
+#          title = "Signature\nz-score",
+#          title_gp = gpar(fontsize = fontsize_plot),
+#          labels_gp = gpar(fontsize = fontsize_plot),
+#          # legend_width = unit(4, "cm"),
+#          # legend_height = unit(4, "cm"),
+#          direction = "vertical"))
+# 
+# file2write <- paste0(dir_out, "heatmap1", ".png")
+# png(file2write, width = 4000, height = 4000, res = 150)
+# p
+# dev.off()
+# 
+# file2write <- paste0(dir_out, "heatmap1", ".pdf")
+# pdf(file2write, width = 15, height = 50, useDingbats = F)
+# draw(object = p,
+#      annotation_legend_side = "left", annotation_legend_list = list_lgd)
+# dev.off()
+# 
+# 
+# 
+# 
