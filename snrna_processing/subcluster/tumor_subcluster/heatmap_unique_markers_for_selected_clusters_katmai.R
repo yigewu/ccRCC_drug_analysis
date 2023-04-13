bd956b2e33089a33a73715d66bd86885bd945b02
yigewu <yigewu2012@gmail.com>
2023-04-13 14:59:18 -0500

    findmarkers for MC2

--- snrna_processing/subcluster/tumor_subcluster/heatmap_unique_markers_for_selected_clusters_katmai.R
+++ snrna_processing/subcluster/tumor_subcluster/heatmap_unique_markers_for_selected_clusters_katmai.R
@@ -52,7 +52,8 @@ print("Finish reading the RDS file!\n")
 
 
 # plot --------------------------------------------------------------------
-p = DoHeatmap(srat, features = c("ABCA1", "C3", "NAV2", "NEAT1", "PLD1", "ROR1"), group.by = "integrated_snn_res.0.5")
-png(paste0(dir_out, "MC2_markers.png"), width = 8, height = 6, res = 150)
+p = DoHeatmap(srat, features = c("ABCA1", "C3", "NAV2", "NEAT1", "PLD1", "ROR1"), 
+              group.by = "integrated_snn_res.0.5")
+png(paste0(dir_out, "MC2_markers.png"), width = 800, height = 600, res = 150)
 print(p)
 dev.off()
